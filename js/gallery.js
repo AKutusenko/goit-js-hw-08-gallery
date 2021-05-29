d9358498e2b41a7626b29410d25c48cb8e805ed3
Artur Kutusenko <kutusenko@gmail.com>
2021-05-30 00:42:32 +0300

    Добавляет навигацию кнопками

--- js/gallery.js
+++ js/gallery.js
@@ -1,68 +1,4 @@
-const images = [
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
-    description: "Hokkaido Flower",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-    description: "Container Haulage Freight",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-    description: "Aerial Beach View",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-    description: "Flower Blooms",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-    description: "Alpine Mountains",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-    description: "Mountain Lake Sailing",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    description: "Alpine Spring Meadows",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    description: "Nature Landscape",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    description: "Lighthouse Coast Sea",
-  },
-];
+import images from "./gallery-items.js";
 
 const refs = {
   gallery: document.querySelector(".js-gallery"),
@@ -81,27 +17,60 @@ createGallery(images);
 function onGalleryClick(e) {
   e.preventDefault();
 
-  if (event.target.classList.contains("gallery__image")) {
+  if (e.target.classList.contains("gallery__image")) {
     refs.lightboxImage.src = e.target.dataset.source;
-    refs.lightboxImage.alt = e.target.dataset.description;
+    refs.lightboxImage.alt = e.target.alt;
     refs.lightbox.classList.toggle("is-open");
   }
 
-  window.addEventListener("keydown", onEscapeKeydown);
+  window.addEventListener("keydown", ifEscapeKeyDown);
+  window.addEventListener("keydown", ifRightKeyDown);
+  window.addEventListener("keydown", ifLeftKeyDown);
 }
 
 function onCloseBtnClick() {
   refs.lightbox.classList.toggle("is-open");
   refs.lightboxImage.src = "";
+  refs.lightboxImage.alt = "";
 }
 
 function onOverlayBtnClick() {
   refs.lightbox.classList.toggle("is-open");
+  refs.lightboxImage.src = "";
+  refs.lightboxImage.alt = "";
 }
 
-function onEscapeKeydown(e) {
+function ifEscapeKeyDown(e) {
   if (e.code === "Escape") {
     refs.lightbox.classList.toggle("is-open");
+    refs.lightboxImage.src = "";
+    refs.lightboxImage.alt = "";
+  }
+}
+
+function ifRightKeyDown(e) {
+  if (e.code === "ArrowRight") {
+    const urls = images.map((image) => image.original);
+    for (let i = 0; i < urls.length; i += 1) {
+      if (urls[i] === refs.lightboxImage.src && i !== 8) {
+        refs.lightboxImage.src = urls[i + 1];
+        console.log(i);
+        break;
+      }
+    }
+  }
+}
+
+function ifLeftKeyDown(e) {
+  if (e.code === "ArrowLeft") {
+    const urls = images.map((image) => image.original);
+    for (let i = 0; i < urls.length; i += 1) {
+      if (urls[i] === refs.lightboxImage.src && i !== 0) {
+        refs.lightboxImage.src = urls[i - 1];
+        console.log(i);
+        break;
+      }
+    }
   }
 }
 
